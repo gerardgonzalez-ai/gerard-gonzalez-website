c933220d7e819950bd9ae032c82d881bbf4089e9
Gerard González <gerardgonzalez.ai@gmail.com>
2023-01-28 23:27:42 -0300

    [] - adding gerard_gonzalez_main app

---
+++ gerard_gonzalez_website_project/gerard_gonzalez_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GerardGonzalezMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gerard_gonzalez_main'
--- gerard_gonzalez_website_project/gerard_gonzalez_website_project/settings.py
+++ gerard_gonzalez_website_project/gerard_gonzalez_website_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gerard_gonzalez_main.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
