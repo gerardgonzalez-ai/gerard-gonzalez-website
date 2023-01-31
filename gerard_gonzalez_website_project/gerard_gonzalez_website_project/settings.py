3dec476b8013e0d395f661b7d5334321083caa09
Gerard González <gerardgonzalez.ai@gmail.com>
2023-01-30 22:43:53 -0300

    [] - first gerard_gonzalez_website's version

--- gerard_gonzalez_website_project/gerard_gonzalez_website_project/settings.py
+++ gerard_gonzalez_website_project/gerard_gonzalez_website_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fontawesomefree',
     'gerard_gonzalez_main.apps.GerardGonzalezMainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
